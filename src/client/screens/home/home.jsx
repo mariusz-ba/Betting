02ddd2dcb481341e2b40a54693f9e5a330f7aaa8
Mariusz Baran <mariusz.baran23@gmail.com>
2018-08-19 23:05:15 +0200

    Render correct home page according to session state

--- src/client/screens/home/home.jsx
+++ src/client/screens/home/home.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import Landing from '../landing/landing';
 import Dashboard from '../dashboard/dashboard';
 
-const isAuthenticated = false;
-
-export default () => {
-  if(isAuthenticated)
+export function Home(props) {
+  if(props.isAuthenticated)
     return <Dashboard/>
   return <Landing/>
 }
+
+function mapStateToProps(state) {
+  return {
+    isAuthenticated: state.session.isAuthenticated
+  }
+}
+
+export default connect(mapStateToProps)(Home);
