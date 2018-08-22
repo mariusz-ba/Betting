d3bf0f5c19068e50c9c03a1180022eb7a21b3ee4
Mariusz Baran <mariusz.baran23@gmail.com>
2018-08-22 09:44:27 +0200

    Create simple event validation

--- src/client/services/events/events.model.js
+++ src/client/services/events/events.model.js
@@ -6,18 +6,73 @@ export class Event {
   constructor(data) {
     const id = get(data, 'id', Event.counter++);
     const name = get(data, 'name', 'unnamed');
-    const organiser = get(data, 'organiser', 0);
+    const organiser = get(data, 'organiser', '0');
     const createdAt = get(data, 'createdAt', Date.now());
     const updatedAt = get(data, 'updatedAt', Date.now());
     const options = get(data, 'options', []);
     const result = get(data, 'result', {});
 
     this.id = id;
-    this.name = name;
-    this.organiser = organiser;
-    this.createdAt = createdAt;
-    this.updatedAt = updatedAt;
-    this.options = options;
-    this.result = result;
+    this.name = this._getName(name);
+    this.organiser = this._getOrganiser(organiser);
+    this.createdAt = this._getCreatedAt(createdAt);
+    this.updatedAt = this._getUpdatedAt(updatedAt);
+    this.options = this._getOptions(options);
+    this.result = this._getResult(result);
+  }
+
+  _getName(value) {
+    if(typeof value !== 'string')
+      return 'unnamed';
+    return value;
+  }
+
+  _getOrganiser(value) {
+    if(typeof value !== 'string')
+      return '0'
+  }
+
+  _getCreatedAt(value) {
+    return typeof value === 'number' ? value : Date.now();
+  }
+
+  _getUpdatedAt(value) {
+    return typeof value === 'number' ? value : Date.now();
+  }
+
+  _getOptions(value) {
+    const defaults = [
+      { id: '0', name: 'unnamed', pool: 0 },
+      { id: '1', name: 'unnamed', pool: 0 }
+    ]
+
+    if(!Array.isArray(value))
+      return defaults;
+    
+    return value.map(item => {
+      if(!item)
+        return defaults[0];
+        
+      return {
+        id: item._id ? item._id : '0',
+        name: item.name ? item.name : 'unnamed',
+        pool: item.pool ? item.pool : 0
+      }
+    })
+  }
+
+  _getResult(value) {
+    const defaults = {
+      finished: false,
+      option: '0'
+    }
+
+    if(typeof value !== 'object')
+      return defaults;
+    
+    return {
+      finished: value.finished ? value.finished : false,
+      option: value.option ? value.option : '0'
+    }
   }
 }
