c0eb6fdc16f7a042646dd781e79de816bbe6bb2b
Mariusz Baran <mariusz.baran23@gmail.com>
2018-08-26 00:31:32 +0200

    Add actions to event displayed on manage page

--- src/client/screens/dashboard/screens/manage/manage.jsx
+++ src/client/screens/dashboard/screens/manage/manage.jsx
@@ -28,26 +28,20 @@ export class Manage extends Component {
             { events &&
               events.map(event => (
                 <li key={event.id}>
-                  {/* <p>{event.name}</p>
-                  <p>{event.options[0].name}: x{event.options[0].multiplier} Total: {event.options[0].pool} Odds: {event.options[0].odds}%</p>
-                  <p>{event.options[1].name}: x{event.options[1].multiplier} Total: {event.options[1].pool} Odds: {event.options[1].odds}%</p>
-                  { event.result.finished === true &&
-                    <p>Closed</p>
-                  }
-                  { event.result.finished === false &&
-                    <div>
-                      <p>Winner</p>
-                      <button onClick={() => this.finish(event.id, event.options[0].id)}>First</button>
-                      <button onClick={() => this.finish(event.id, event.options[1].id)}>Second</button>
-                    </div>
-                  } */}
                   <Event
                     id={event.id}
                     name={event.name}
                     date={event.createdAt}
                     finished={event.result.finished}
                     winner={event.result.option}
-                    options={event.options} />
+                    options={event.options} >
+                    { event.result.finished === false &&
+                      <Event.Actions>
+                        <button onClick={() => this.finish(event.id, event.options[0].id)}>First</button>
+                        <button onClick={() => this.finish(event.id, event.options[1].id)}>Second</button>
+                      </Event.Actions>  
+                    }
+                  </Event>
                 </li>
               )) 
             }
