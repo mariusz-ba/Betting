df6db06f5b0ad4cb638f807348fd4f672181c3a2
Mariusz Baran <mariusz.baran23@gmail.com>
2018-08-25 17:46:19 +0200

    Calculate odds for each event

--- src/client/screens/dashboard/screens/events/selectors.js
+++ src/client/screens/dashboard/screens/events/selectors.js
@@ -1,4 +1,5 @@
 import { createSelector } from 'reselect';
+import { calculateOdds } from '@services/events/events.utils';
 
 const bets = state => state.bets;
 const events = state => state.events;
@@ -8,8 +9,15 @@ export const eventsSelector = createSelector(
   bets,
   (events, bets) => Object.values(events.events).map(event => {
     const bet = Object.values(bets.bets).find(bet => bet.event === event.id);
+    const odds = calculateOdds(event.options);
+
+    const eventWithOdds = {
+      ...event,
+      options: odds
+    }
+
     if(bet)
-      return { ...event, userPick: bet.option };
-    return { ...event, userPick: null }
+      return { ...eventWithOdds, userPick: bet.option };
+    return { ...eventWithOdds, userPick: null }
   })
 )
